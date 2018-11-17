cb29f6f3a15ba9bf21ff1bfac1433712d4cb8da5
Keven Parent <lenigma444@hotmail.com>
2018-11-17 02:24:13 -0500

    Added create expense_category

--- app/controllers/expense_categories_controller.rb
+++ app/controllers/expense_categories_controller.rb
@@ -1,4 +1,24 @@
 class ExpenseCategoriesController < ApplicationController
   def index
+    @expense_categories = ExpenseCategory.all
+  end
+
+  def create
+    @expense_category = ExpenseCategory.new(permited_params)
+    @save_state = @expense_category.save
+
+    if @save_state 
+      @expense_categories = ExpenseCategory.all
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end 
+
+  private 
+
+  def permited_params
+    params.permit(:name)
   end
 end
