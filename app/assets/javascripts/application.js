e48774cea1ece5da865cd7f14974d3067055126e
Keven Parent <lenigma444@hotmail.com>
2018-11-16 23:54:56 -0500

    Added dashboard design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,31 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require Chart.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+////
+// Open sidemenu
+
+$(document).ready(function (event) {
+  $('.navbar__sideMenuButton').click(function (event) {
+    if ($('.sideBar').hasClass('active')) {
+      $('.sideBar').removeClass('active');
+    } else {
+      $('.sideBar').addClass('active');
+    }
+  });
+});
+
+
+////
+// Close sidemenu
+$(document).click(function (event) {
+  if (!$(event.target).closest('.sideBar').length && !$(event.target).is($('#sideBarBtn'))) {
+    $('.sideBar').removeClass('active');
+  }
+});
