7393940b0c02e4c632fd883ae26cba4a221fd176
Keven Parent <lenigma444@hotmail.com>
2018-11-18 02:02:19 -0500

    Added real data to graphs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
     @expense_sum = Expense.all.sum(:amount)
     @income_sum = Income.all.sum(:amount)
     @balance = @income_sum - @expense_sum
+    @expense_categories = ExpenseCategory.all
   end
 end
