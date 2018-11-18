753cbb61a953eb7321ca2c0b15c607d36a6fb7bd
Keven Parent <lenigma444@hotmail.com>
2018-11-18 00:58:33 -0500

    Added income crud

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,4 +1,60 @@
 class IncomesController < ApplicationController
   def index
+    @incomes = Income.all
+    puts @incomes
+    @new_income = Income.new
+  end
+
+  def create 
+    @income = Income.new(permited_params)
+    @save_state = @income.save
+
+    if @save_state 
+      @incomes = Income.all
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit
+    @income = Income.find(params[:id])
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update
+    @income = Income.find(params[:id])
+    @update_state = @income.update(permited_params)
+
+    if @update_state 
+      @incomes = Income.all
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+
+    if @income.destroyed?
+      @incomes = Income.all
+    end
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private 
+
+  def permited_params
+    params.require(:income).permit(:name, :amount)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_040920) do
+ActiveRecord::Schema.define(version: 2018_11_18_053935) do
 
   create_table "expense_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2018_11_18_040920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["expense_category_id"], name: "index_expenses_on_expense_category_id"
+  end
+
+  create_table "incomes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
